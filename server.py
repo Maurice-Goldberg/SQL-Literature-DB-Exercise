01568e5eb0f4175e541c2d69274afc9d948b68c3
mg3183 <mg3183@columbia.edu>
2016-06-18 22:52:06 +0000

    small changes

--- server.py
+++ server.py
@@ -69,7 +69,6 @@ def teardown_request(exception):
 def index():
   return render_template("index.html")
 
-"""
 @app.route('/selection')
 def selection():
 	select = request.form.get('category')
@@ -82,35 +81,6 @@ def selection():
 	  	context = dict(data = names)
 	return render_template("/selection.html", **context)
 
-  #
-  # Flask uses Jinja templates, which is an extension to HTML where you can
-  # pass data to a template and dynamically generate HTML based on the data
-  # (you can think of it as simple PHP)
-  # documentation: https://realpython.com/blog/python/primer-on-jinja-templating/
-  #
-  # You can see an example template in templates/index.html
-  #
-  # context are the variables that are passed to the template.
-  # for example, "data" key in the context variable defined below will be 
-  # accessible as a variable in index.html:
-  #
-  #     # will print: [u'grace hopper', u'alan turing', u'ada lovelace']
-  #     <div>{{data}}</div>
-  #     
-  #     # creates a <div> tag for each element in data
-  #     # will print: 
-  #     #
-  #     #   <div>grace hopper</div>
-  #     #   <div>alan turing</div>
-  #     #   <div>ada lovelace</div>
-  #     #
-  #     {% for n in data %}
-  #     <div>{{n}}</div>
-  #     {% endfor %}
-  #  
-"""
-
-"""
 @app.route('/result')
 def result():
   if(select == "Author"):
@@ -129,7 +99,6 @@ def result():
   	return render_template("/structureresult.html", **context)
   if(select == "Institution"):
   	return render_template("/institutionresult.html", **context)
-"""
 
 """ 
   request is a special object that Flask provides to access web request information:
